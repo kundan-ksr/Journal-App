9f868308c4b7a277d8e2050275c829c8e033a5ec
Kundan kr <shivaayssr@gmail.com>
2026-08-12 13:03:55 +0530

    add Public Controller for open access

--- src/main/java/com/learnspringboot/myJournalApp/controller/PublicController.java
+++ src/main/java/com/learnspringboot/myJournalApp/controller/PublicController.java
@@ -17,7 +17,7 @@ public class PublicController {
         return "OK";
     }
 
-    @PostMapping
+    @PostMapping("/create-user")
     public void createUser(@RequestBody User user){
         userService.saveEntry(user);
     }
